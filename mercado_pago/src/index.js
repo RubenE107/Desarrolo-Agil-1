ac70c9667323430a036588568e1bc00b04f5e456
RubenE107 <irtu098@gmail.com>
2025-03-30 17:40:59 -0600

    c4redenciales cors

--- mercado_pago/src/index.js
+++ mercado_pago/src/index.js
@@ -2,10 +2,18 @@ import express from 'express';
 import pagoRoutes from './routes/pagoRoutes.js';
 import { PORT } from './config.js';
 import morgan from 'morgan';
+import cors from 'cors';
 
 
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:4200', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200 // For legacy browser support
+  }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(pagoRoutes);
